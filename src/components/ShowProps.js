2ecbd90a2d99f90f939e576ea1a4f4ce72d38792
Jbin <kjb7907@naver.com>
2018-06-30 13:44:25 +0900

    props

---
+++ src/components/ShowProps.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ShowProps extends Component {
+    render() {
+        let style = {
+            textAlign: 'center'
+        }
+        return (
+            <div>
+                <div style={style}>
+                    <span>부모 컴포넌트에서 받은 name: {this.props.name}</span>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ShowProps;
